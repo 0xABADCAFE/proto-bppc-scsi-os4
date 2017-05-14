89e8e23bd6aaee34040aa13ce760cffda630ccb7
Hamza <karlchurchill@gmail.com>
2017-05-14 22:16:53 +0100

    Debugging macro.

---
+++ src/debug.c
@@ -0,0 +1,16 @@
+#include <proto/exec.h>
+#include "debug.h"
+
+#ifdef DEBUG
+
+/* Error level strings for debugging */
+const char * BPPC_DebugLevel[DBG_MISC+1] = {
+	"Critical",
+	"Error",
+	"Warning",
+	"Notice",
+	"Debug",
+	"Misc"
+};
+
+#endif
---
+++ src/debug.h
@@ -0,0 +1,24 @@
+#ifndef _OS4_BLIZZPPC_DEVICE_DEBUG_H_
+	#define _OS4_BLIZZPPC_DEVICE_DEBUG_H_
+
+	#define DBG_CRITICAL 0  /* Most severe */
+	#define DBG_ERROR    1  /* serious problem */
+	#define DBG_WARN     2  /* warning */
+	#define DBG_NOTICE   3  /* notification */
+	#define DBG_INFO     4  /* basic debug information */
+	#define DBG_MISC     5  /* miscellaneous extra information */
+
+	#ifdef DEBUG
+
+		extern const char * BPPC_DebugLevel[DBG_MISC+1];
+
+		/* pprintf() macro - pretty print for debugging */
+		#define pprintf(level, format, args...) if ((level)<=DEBUGLEVEL) ((struct ExecIFace *)((*(struct ExecBase **)4)->MainInterface))->DebugPrintF("[%-8s] [%s] " format, BPPC_DebugLevel[(level)], __PRETTY_FUNCTION__ , ## args)
+
+	#else
+
+		#define pprint(level, format, args...)
+
+	#endif
+
+#endif

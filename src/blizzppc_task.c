3854c137227204eb71e690d6b4a20ee6e8990b2a
kchurchill <karl.churchill@gear4music.com>
2017-05-15 19:00:57 +0100

    Implemented basic sub task loop. It doesn't do anything yet other than ignore whatever messages arrive at the command port.

--- src/blizzppc_task.c
+++ src/blizzppc_task.c
@@ -10,8 +10,37 @@
 #include <exec/ports.h>
 #include <exec/tasks.h>
 
-extern struct ExecIFace * IExec;
-
 void BlizzPPC_TaskMain(BlizzPPC_Device * bppc_Device) {
+	uint32 expectSignals, receivedSignals;
+
+	union {
+		struct Message  * message;
+		struct IOStdReq * ioRequest;
+	} command;
+
+	/* Define the signals we will wait on */
+	expectSignals =
+		(1UL << bppc_Device->bpd_CommandPort->mp_SigBit) |
+		(1UL << bppc_Device->bpd_TimerPort->mp_SigBit) |
+		(1UL << bppc_Device->bpd_IntSignalBit)
+	;
+
 	pprintf(DBG_NOTICE, "Here\n");
+
+	do {
+		int intCommandCount = 0;
+		pprintf(DBG_INFO, "Waiting for signals %08X\n", (unsigned)expectSignals);
+		receivedSignals = IExec->Wait(expectSignals);
+		pprintf(DBG_INFO, "Received signals %08X\n", (unsigned)receivedSignals);
+
+		/* For now, just reply to everything with failure */
+		while ( (command.message = IExec->GetMsg(bppc_Device->bpd_CommandPort)) ) {
+			command.ioRequest->io_Error = IOERR_OPENFAIL;
+			IExec->ReplyMsg(command.message);
+			++intCommandCount;
+		}
+		pprintf(DBG_INFO, "Ignored %d messages received by the CommandPort", intCommandCount);
+
+	} while (1);
 }
+
